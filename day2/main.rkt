71b69ef5a1ed8a58a349e0dff6bf8c5c7a9631cc
kimierik <kimi.malkamaki@gmail.com>
2025-12-02 20:55:31 +0200

    cleanup

--- day2/main.rkt
+++ day2/main.rkt
@@ -1,12 +1,9 @@
 #lang racket
 
+(define (get_data fname) (string-split (read-line (open-input-file fname)) ","))
 
-(define (get_data fname) 
-  (define port (open-input-file fname))
-  (define d (read-line port))
-  (string-split d ",")
-)
-
+(define atoi string->number )
+(define nts number->string )
 
 (define (id_is_invalid id) 
   (define l (string-length id))
@@ -16,39 +13,21 @@
             (
               [left (substring id 0 (/ l 2))]
               [right (substring id (/ l 2))]
-            )
-            (if (string=? left right) (println id) '())
-           (string=? left right)
-           )]
+            ) (string=? left right))]
     )
-
   )
 
 (define (get_invalid_ids r) 
     (apply + 
       (map string->number 
-        (filter id_is_invalid 
-          (map number->string r)
-        )
-      )
-    )
-  )
-
-(define atoi string->number )
-(define nts number->string )
+        (filter id_is_invalid (map number->string r))
+      )))
 
 (define (get_range_from_string s)
   (define l (string-split s "-"))
   (range (atoi (car l)) (add1 (atoi (car(cdr l)))))
 )
 
-(define (get_total_invalids data)
-  (apply + (map get_invalid_ids (map get_range_from_string data)))
-)
+(define (get_total_invalids data) (apply + (map get_invalid_ids (map get_range_from_string data))))
 
-
-(define data (get_data "input.txt"))
-(println data)
-(get_total_invalids data)
-
-
+(get_total_invalids (get_data "input.txt"))
